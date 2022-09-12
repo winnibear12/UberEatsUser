5e542b83ceef7f7a251a6590554994ad8f48e968
Sangmin Lee (Simon) <winnibear12@gmail.com>
2022-09-12 14:31:32 -0500

    Added restaurant detailed screen

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet,View} from 'react-native';
 import HomeScreen from './src/screens/HomeScreen';
+import ReataurantDetailsScreen from './src/screens/RestaurantDetailsScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <HomeScreen />
-      <StatusBar style="auto" />
+      <ReataurantDetailsScreen />
+      {/* <HomeScreen /> */}
+      <StatusBar style="light" />
+
+  
       
     </View>
   );
@@ -18,8 +22,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    padding:10,
-    paddingVertical:30,
+    // padding:10,
+    // paddingVertical:30,
   },
  
 }); 
