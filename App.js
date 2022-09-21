a41219a920cfc2aa6bcfbdf88d521c598215ba7b
Sangmin Lee (Simon) <winnibear12@gmail.com>
2022-09-21 15:33:39 -0500

    Setting up backend with AWS amplify

--- App.js
+++ App.js
@@ -2,18 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet,View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import RootNavigator from './src/navigation';
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react-native/dist/Auth';
+import config from './src/aws-exports';
+Amplify.configure(config);
 
-export default function App() {
+function App() {
   return (
     <NavigationContainer>   
-       
       <RootNavigator />
      
 
       <StatusBar style="light" />    
-
     </NavigationContainer>
 
   );
 }
+
+export default withAuthenticator(App);
 
